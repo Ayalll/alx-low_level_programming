9d23164e9b10770ccc05447b7843eeef29ba9954
Ayalll <ayouyayouya13@gmail.com>
2023-06-26 13:59:06 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "maim.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
